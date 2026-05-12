24dad5b9b5c010c9ec3dce03db9dd9698d851fbc
alena-roth <alena.roth2@gmail.com>
2026-05-12 18:46:12 +0200

    2.3 working (test session life more)

--- server/files/edit.js
+++ server/files/edit.js
@@ -3,7 +3,7 @@ function setMovie(movie) {
   for (const element of document.forms[0].elements) {
     const name = element.id;
     const value = movie[name];
-    
+
     if (name === "Genres") {
       const options = element.options;
       for (let index = 0; index < options.length; index++) {
@@ -36,7 +36,7 @@ function getMovie() {
         }
       }
     } else if (name === "Metascore" || name === "Runtime" || name === "imdbRating") {
-        value = Number(element.value);
+      value = Number(element.value);
     } else if (name === "Actors" || name === "Directors" || name === "Writers") {
       value = element.value.split(",").map((item) => item.trim());
     } else {
@@ -53,14 +53,14 @@ function putMovie() {
   const movie = getMovie();
 
   const xhr = new XMLHttpRequest()
-  xhr.onload = function() {
+  xhr.onload = function () {
     if (xhr.status == 200 || xhr.status === 204) {
       location.href = 'index.html'
     } else {
       alert("Saving of movie data failed. Status code was " + response.status)
     }
   }
-  
+
   xhr.open("PUT", "/movies/" + movie.imdbID)
   xhr.setRequestHeader("Content-Type", "application/json")
 
@@ -73,12 +73,12 @@ const imdbID = new URLSearchParams(window.location.search).get("imdbID");
 
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "/movies/" + imdbID);
-xhr.onload = function() {
+xhr.onload = function () {
   if (xhr.status === 200) {
     setMovie(JSON.parse(xhr.responseText));
   } else {
     alert("Loading of movie data failed. Status was " + xhr.status + " - " + xhr.statusText);
-  } 
+  }
 }
 
 xhr.send()
